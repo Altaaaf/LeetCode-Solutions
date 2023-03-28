f4d9f7acbd9d2c0a0dffd658e3f55f1ef9de5feb
Altaaaf <75543185+Altaaaf@users.noreply.github.com>
2023-03-28 16:24:25 -0400

    Add files via upload

---
+++ Python/Median_of_Two_Sorted_Arrays.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        merged_array = nums1 + nums2
+        merged_array.sort()
+        len_merged_array = len(merged_array)
+        middle_number = len_merged_array//2
+        if len_merged_array % 2 == 0:
+            return (merged_array[middle_number] + merged_array[middle_number-1])/2
+        return merged_array[middle_number]
