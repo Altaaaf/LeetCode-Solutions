0e430b8d002c4623a57e5e99e8cd2652c81f125b
Altaaaf <75543185+Altaaaf@users.noreply.github.com>
2023-03-28 15:54:10 -0400

    Add files via upload

---
+++ Python/Running_Sum_of_1d_array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        new_array = []
+        current_total = 0
+
+        for num in nums:
+            current_total += num
+            new_array.append(current_total)
+
+        return new_array
